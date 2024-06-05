91c5b59b160bcf90d6904f6d87690c5c1b42404d
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-04 21:52:27 -0300

    up

--- Sapore/assets/js/main.js
+++ Sapore/assets/js/main.js
@@ -13,3 +13,49 @@ const showMenu = (toggleId, navId) =>{
  }
  
  showMenu('nav-toggle','nav-menu')
+
+ /*Slide Show */
+
+ const myslider = document.querySelectorAll('.myslider'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslider.length;i++){
+		myslider[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslider.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslider[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
