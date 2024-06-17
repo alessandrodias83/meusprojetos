a9f7fb839e175de1dca41b1a9e0c6c9637f76066
alessandrodias83 <158523798+alessandrodias83@users.noreply.github.com>
2024-06-17 12:57:04 -0300

    up

---
+++ paralax/script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area1', {duration:1000});
+
+sr.reveal('.area2', {
+    rotate: {x:0, y:80, z:0},
+    duration:2000,
+});
+
+sr.reveal('.area3', {
+    rotate: {x: 100, Y:100, z:0},
+    duration:2000,
+});
