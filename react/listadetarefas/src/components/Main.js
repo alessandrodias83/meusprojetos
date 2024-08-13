f7746b475757c1bd39a7b2589155570206161f43
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-08-12 23:16:12 -0300

    react lista de tarefas

---
+++ react/listadetarefas/src/components/Main.js
@@ -0,0 +1,54 @@
+import React, { Component, component } from 'react';
+
+//Form
+import {FaPlus} from 'react-icons/fa';
+
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+      novaTarefa: '',
+      tarefas: [
+        'Fazer café',
+        'Beber água',
+        'Estudar',
+      ],
+    };
+
+  handleChange = (e) => {
+    this.setState({
+      novaTarefa: e.target.value,
+    });
+  }
+
+  render() {
+    const { novaTarefa, tarefas } = this.state;
+
+    return (
+      <div className='main'>
+        <h1>Lista de Tarefas</h1>
+
+        <form action='#' className='form'>
+          <input
+          onChange={this.handleChange}
+          type='text'
+          value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className='tarefas'>
+          {tarefas.map(tarefa => (
+            <li>{tarefa}</li>
+          )
+
+          )}
+        </ul>
+
+      </div>
+    )
+  }
+}
+
