a36320e2a29a56a71edb82a79d59f35cd0761009
Alessandro <158523798+alessandrodias83@users.noreply.github.com>
2024-08-14 23:00:22 -0300

    up

---
+++ react/react-create/src/pages/Login/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <> 
+    <Container>
+    <Title>
+      Login
+      <small>Oie</small>
+    </Title>
+    <p>Loren ips test kdjsld</p>
+    </Container>
+    </>
+  
+  );
+}
